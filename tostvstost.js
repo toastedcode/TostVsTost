06f157b2a6a3dd1501006fb93675bacb9f3536c5
toastedcode <jasontost@gmail.com>
2017-12-06 16:17:12 -0500

    Initial commit

---
+++ tostvstost.js
@@ -0,0 +1,21 @@
+function keydown(element)
+{
+   switch (element.id)
+   {
+      case "up-button":
+      {
+         break;
+      }
+   }
+}
+
+function keyup(element)
+{
+   switch (element.id)
+   {
+      case "up-button":
+      {
+         break;
+      }
+   }
+}
